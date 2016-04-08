0d5624c8f0ff68e60df7bc34e6f72d130e3b4545
Modesto Figuereo Fernandez <figuereo.fernandez.modesto@gmail.com>
2016-04-07 22:35:00 -0400

    Add closure.js file.

---
+++ closure.js
@@ -0,0 +1,46 @@
+function Person(_firstName, _lastName, _age) {
+  let firstName = _firstName;
+  let lastName = _lastName;
+  let age = _age;
+
+  const methods = new Array();
+
+  methods['getFullName'] = function() {
+    return firstName + ' ' + lastName;
+  }
+
+  methods['getFirstName'] = function() {
+    return firstName;
+  }
+
+  methods['setFirstName'] = function(value) {
+    firstName = value;
+  }
+
+  methods['getLastName'] = function() {
+    return lastName;
+  }
+
+  methods['setLastName'] = function(value) {
+    lastName = value;
+  }
+
+  methods['getAge'] = function() {
+    return age;
+  }
+
+  methods['setAge'] = function(value) {
+    age = value;
+  }
+
+  methods['isUnderAge'] = function() {
+    return age < 18;
+  }
+
+  return methods;
+}
+
+const juan = Person('Juan', 'Perez', 25);
+console.log(juan.getFullName()); // Juan Perez
+console.log(juan['getFullName']()); // Juan Perez
+console.log('Is juan under age? ' + juan.isUnderAge());
